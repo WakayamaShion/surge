fd816f1aa2f2b7c9eb661f4e687401c608029046
WakayamaShion <62602962+WakayamaShion@users.noreply.github.com>
2023-06-03 11:34:30 +0800

    更新 Comic.js

--- Comic.js
+++ Comic.js
@@ -1,3 +1,4 @@
+# １８Ｃｏｍｉｃ
 var body = $response.body
     .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/18comic.css" type="text/css">');
 $done({ body });
