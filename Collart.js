cd85f5b7864513b587fc3af3d2999198a2e81697
WakayamaShion <62602962+WakayamaShion@users.noreply.github.com>
2023-06-03 11:31:00 +0800

    更新 Comic.js

--- Collart.js
+++ Collart.js
@@ -1,23 +1,3 @@
-var body = JSON.parse($response.body);
-
-body = {
-  "entitlements" : [
-    {
-      "redeem" : {
-
-      },
-      "expires_date_ms" : 4092599349000,
-      "purchase_date_ms" : 1671198216000,
-      "product_identifier" : "Collart_Promium_Yearly_New_20220808",
-      "is_in_intro_offer_period" : false,
-      "environment" : "Production",
-      "auto_renew" : false,
-      "is_in_trial_period" : true,
-      "entitlement_id" : "premium"
-    }
-  ],
-  "is_valid" : true
-}
-
-
-$done({body : JSON.stringify(body)});
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/18comic.css" type="text/css">');
+$done({ body });
